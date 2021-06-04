f1f1d5e8a521486be73d2e085e6b6e8ec45efdde
giada <giadainnocenti.in@gmail.com>
2021-06-03 23:07:07 -0400

    I am seeing the end

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle
 import pandas as pd
 from wine_ML import wine_quality, wine_type, plotly_figure_type, new_dataframe 
 from flask import Flask, render_template, request
-from os import path
+from os import path, remove
 
 
 app = Flask(__name__)
@@ -32,14 +32,17 @@ df_init = pd.read_csv(f'lib/data/winequality-final.csv')
 ############## ONLINE APPLICATION
 @app.route('/')
 def home():
+    remove('lib/data/winequality-flask.csv')
     return(render_template('home.html'))
 
 @app.route('/about')
 def about():
+    remove('lib/data/winequality-flask.csv')
     return(render_template('about.html'))
 
 @app.route('/data_analysis')
 def data_analysis():
+    remove('lib/data/winequality-flask.csv')
     return(render_template('analysis.html'))
 
 @app.route('/prediction', methods=['GET', 'POST'])
@@ -83,7 +86,7 @@ def prediction():
             result[descriptor] = model_dic[f'kNN_{descriptor}'].predict(model_dic[f'pca_{descriptor}'].transform(data))
         wine_result = f"{wine_type(result['type_red']).capitalize()} wine of {wine_quality(result['quality'])}"
         graphJson = plotly_figure_type(X_df, model_dic['pca_type_red'].transform(data), df['type'])
-        data['type'] = result['type_red']
+        data['type'] = wine_type(result['type_red'])
         data['quality'] = result['quality']
         df2 = new_dataframe(df, data)
         df2.to_csv(f'lib/data/winequality-flask.csv',index=False)
